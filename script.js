b539dcaf5765f05b2ac8bd503b7ddc555632d875
brypolanco <brpolanc@gmail.com>
2022-02-01 21:58:42 -0500

    Remove input DOM elements and return objects in buttons module

--- script.js
+++ script.js
@@ -15,8 +15,10 @@ const gameBoard = (()=>{
             }
         }
     };
-    buildBoard(3);
-    console.log(items);
+    const boardDOM = buildBoard(3);
+    const getItems = ()=>{items};
+
+    console.log(getItems);
 })();
 
 
@@ -28,45 +30,20 @@ const Player = (name, sign) =>{
 //UI Buttons module
 const buttons = (()=>{
     //DOM elements
-    const inputDOM = (player)=>{
-        const inputArea = document.createElement('input');
-        inputArea.placeholder = `Player ${player}`;
-        document.body.appendChild(inputArea).id = `player${player}-input`;
-        return inputArea;
-    };
-
-    const removeDOM = ()=>{
-        if(player1 && player2){
-            player1Area.remove();
-            player2Area.remove();
-            startDOM();
-        }
-    }
-
     const startDOM = ()=>{
         const startButton = document.createElement('button');
         startButton.textContent = 'START';
         document.body.appendChild(startButton).id = 'start-button';
+        return startButton;
     }
-
-    const player1Area = inputDOM(1);
-    const player2Area = inputDOM(2);
-
+    
+    const startButton = startDOM();
 
     //Event Listeners
-    let player1 = player1Area.addEventListener('keypress', (e)=>{
-        if (e.key === 'Enter') {
-            player1 = Player(e.target.value, 'X');
-            console.log(player1)
-            removeDOM();
-        }
-    })
+    startButton.addEventListener('click', gameLogic)
 
-    let player2 = player2Area.addEventListener('keypress', (e)=>{
-        if (e.key === 'Enter') {
-            player2 = Player(e.target.value, 'O');
-            console.log(player2)
-            removeDOM();
-        }
-    })
+    let player1 = Player('Player 1', 'X');
+    let player2 = Player('Player 2', 'O');
+
+    return{player1, player2};
 })();
